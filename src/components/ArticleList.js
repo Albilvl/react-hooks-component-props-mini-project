6cad05efcbaf982a2fc383f1c8a57036917872b6
Al Bilal <bil.bank@yahoo.com>
2022-02-02 11:29:19 -0500

    commit tree 8d24b69cecb582444aa07592b91d94dc651e85e0

---
+++ src/components/ArticleList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Article from './Article'
+
+
+
+export default function ArticleList({posts}) {
+    const articleArray = posts.map(post =>{
+        return(
+          <Article
+          key = {post.id}
+          title = {post.title}
+          date = {post.date}
+          preview = {post.preview}
+          minutes = {post.minutes}
+          />
+      
+    )
+    })
+    return <main>{articleArray}</main>
+  
+}
+
